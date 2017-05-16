a3a7f987660290d17ca873284894ff934555dbb2
Adam <adam8690@hotmail.co.uk>
2017-05-16 09:39:24 +0100

    cretaed a blue world  and added a ball sprite to the center of it

--- public/app.js
+++ public/app.js
@@ -4,18 +4,29 @@ window.onload = function() {
         //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
         //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
 
-        var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+        var game = new Phaser.Game(500, 500, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+// called first
         function preload () {
-
-            game.load.image('logo', 'phaser.png');
-
+            // load image assets before game starts
+            game.load.image('ball', 'ball.png');
         }
-
+// called after preload
         function create () {
+            // center the game on the page
+            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            game.scale.pageAlignHorizontally = true;
+            game.scale.pageAlignVertically = true;
 
-            var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-            logo.anchor.setTo(0.5, 0.5);
+            // change background colour
+            game.stage.backgroundColor = '#87CEEB';
+
+            // add ball to middle of game area
+            game.add.sprite(game.world.centerX, game.world.centerY, 'ball');
+            this.ball.anchor.set(0.5, 0.5);
+        }
+// called once every frame (60hz)
+        function update () {
 
         }
 
