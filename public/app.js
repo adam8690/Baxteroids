688ee3e0b2a9768ace6c218cc950698dbf645731
Adam <adam8690@hotmail.co.uk>
2017-05-16 09:59:58 +0100

    ball now moves around and cannot leave the boundaries of the canvas

--- public/app.js
+++ public/app.js
@@ -6,6 +6,10 @@ window.onload = function() {
 
         var game = new Phaser.Game(500, 500, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+        var BallWorld = {
+            velocity: 8
+        };
+
 // called first
         function preload () {
             // load image assets before game starts
@@ -22,12 +26,44 @@ window.onload = function() {
             game.stage.backgroundColor = '#87CEEB';
 
             // add ball to middle of game area
-            game.add.sprite(game.world.centerX, game.world.centerY, 'ball');
+            this.ball = game.add.sprite(game.world.centerX, game.world.centerY, 'ball');
             this.ball.anchor.set(0.5, 0.5);
+            
+            // add key input to the game
+            this.keys = game.input.keyboard.createCursorKeys();
         }
 // called once every frame (60hz)
         function update () {
+        // poll arrow keys to move the ball
+        if(this.keys.left.isDown){
+            this.ball.x -= BallWorld.velocity
+        }
+        if(this.keys.right.isDown){
+            this.ball.x += BallWorld.velocity
+        }
+        if(this.keys.down.isDown){
+            this.ball.y += BallWorld.velocity
+        }
+        if(this.keys.up.isDown){
+            this.ball.y -= BallWorld.velocity
+        }
+        
+        // prevent the ball from leaving the boundaries of the canvas
+        var halfWidth = this.ball.width / 2
+        var halfHeight = this.ball.height / 2
 
+        if((this.ball.x - halfWidth) < 0){
+            this.ball.x = halfWidth;
+        }
+        if((this.ball.x + halfWidth) > game.width){
+            this.ball.x = game.width - halfWidth;
+        }
+        if((this.ball.y - halfHeight) < 0){
+            this.ball.y = halfHeight;
+        }
+        if((this.ball.y + halfHeight) > game.height){
+            this.ball.y = game.height - halfHeight;
+        }
         }
 
     };
