4f05cf8f54345d1be2c58755c71309275b8a0c36
Adam <adam8690@hotmail.co.uk>
2017-05-16 10:02:05 +0100

    tidied up comments

--- public/app.js
+++ public/app.js
@@ -1,9 +1,5 @@
 window.onload = function() {
 
-        //  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
-        //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
-        //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
-
         var game = new Phaser.Game(500, 500, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
         var BallWorld = {
@@ -34,36 +30,36 @@ window.onload = function() {
         }
 // called once every frame (60hz)
         function update () {
-        // poll arrow keys to move the ball
-        if(this.keys.left.isDown){
-            this.ball.x -= BallWorld.velocity
-        }
-        if(this.keys.right.isDown){
-            this.ball.x += BallWorld.velocity
-        }
-        if(this.keys.down.isDown){
-            this.ball.y += BallWorld.velocity
-        }
-        if(this.keys.up.isDown){
-            this.ball.y -= BallWorld.velocity
-        }
-        
+            // poll arrow keys to move the ball
+            if(this.keys.left.isDown){
+                this.ball.x -= BallWorld.velocity
+            }
+            if(this.keys.right.isDown){
+                this.ball.x += BallWorld.velocity
+            }
+            if(this.keys.down.isDown){
+                this.ball.y += BallWorld.velocity
+            }
+            if(this.keys.up.isDown){
+                this.ball.y -= BallWorld.velocity
+            }
+            
         // prevent the ball from leaving the boundaries of the canvas
         var halfWidth = this.ball.width / 2
         var halfHeight = this.ball.height / 2
 
-        if((this.ball.x - halfWidth) < 0){
-            this.ball.x = halfWidth;
-        }
-        if((this.ball.x + halfWidth) > game.width){
-            this.ball.x = game.width - halfWidth;
-        }
-        if((this.ball.y - halfHeight) < 0){
-            this.ball.y = halfHeight;
-        }
-        if((this.ball.y + halfHeight) > game.height){
-            this.ball.y = game.height - halfHeight;
-        }
+            if((this.ball.x - halfWidth) < 0){
+                this.ball.x = halfWidth;
+            }
+            if((this.ball.x + halfWidth) > game.width){
+                this.ball.x = game.width - halfWidth;
+            }
+            if((this.ball.y - halfHeight) < 0){
+                this.ball.y = halfHeight;
+            }
+            if((this.ball.y + halfHeight) > game.height){
+                this.ball.y = game.height - halfHeight;
+            }
         }
 
     };
