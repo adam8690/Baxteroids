735467ec82d77da3e6bcf6226d28a3bb816a40c0
Adam <adam8690@hotmail.co.uk>
2017-05-17 16:17:27 +0100

    Game Over screen now displays in the center of the screen.

--- public/ship.js
+++ public/ship.js
@@ -5,7 +5,7 @@ function Ship(game){
     this.drag = 100;
     this.maxVelocity = 400;
     this.sprite = null;
-    this.startingLives = 2;
+    this.startingLives = 3;
     this.livesText = null;
     this.timeToReset = 3;
 }
@@ -24,11 +24,20 @@ Ship.prototype.resetShip = function(){
     this.sprite.reset(this.game.world.centerX, this.game.world.centerY);
 }
 
+Ship.prototype.gameOver = function(){
+    var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" }
+    var gameOverText = this.game.add.text(this.game.world.centerX, this.game.world.centerY, "Game Over", style)
+    gameOverText.anchor.set(0.5)
+}
+
 // called when the ship hits an asteroid
 Ship.prototype.destroy = function(){
     this.startingLives --;
     this.livesText.setText(this.startingLives)
-    if(this.startingLives){
+    if(this.startingLives > 0){
         this.resetShip();
+    }
+    else{
+        this.gameOver();
     }
 }
